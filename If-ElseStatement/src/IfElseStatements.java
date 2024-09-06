c3e5d5b22b91b3cfbf20cf77b6db957b0c6905cc
Mateo Rendon MacBook <rendoncode@gmail.com>
2024-09-06 00:04:48 -0600

    5 primeros retos

---
+++ If-ElseStatement/src/IfElseStatements.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class IfElseStatements {
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        System.out.println("Introduce un numero: ");
+        int N = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+        if (N % 2 != 0){
+            System.out.println("Weird");
+        }else if (N % 2 == 0 && (6 <= N && N <= 20)){
+            System.out.println("Weird");
+        }else if (N % 2 == 0 && (2 <= N && N >= 5)){
+            System.out.println("Not Weird");
+        }else{
+            System.out.println("Not Weird");
+        }
+
+        scanner.close();
+    }
+}
